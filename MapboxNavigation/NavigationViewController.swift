staticdreams/mapbox-navigation-ios

Use target point and animate if plugged in

MapboxNavigation/NavigationViewController.swift
@@ -404,7 +404,8 @@ extension NavigationViewController: RouteControllerDelegate {
             return
         }
         
-        mapViewController?.mapView.userLocationForCourseTracking = mapViewController?.locationForCourseTracking(derivedFrom: location)
+        let courseTrackingLocation = mapViewController?.locationForCourseTracking(derivedFrom: location)
+        mapViewController?.mapView.updateCourseTracking(location: courseTrackingLocation, animated: routeController.locationManager.isPluggedIn)
         mapViewController?.updateLabels(for: location)
     }
 }

sw-code/flitsmeister-navigation-ios

MapboxNavigation/NavigationViewController.swift
@@ -610,6 +610,7 @@ extension NavigationViewController: RouteControllerDelegate {
     
     public func routeController(_ routeController: RouteController, didUpdate locations: [CLLocation]) {
         if let location = locations.last {
+            mapViewController?.mapView.updateCourseTracking(location: location, animated: routeController.locationManager.isPluggedIn)
             mapViewController?.mapView.userLocationForCourseTracking = location
             mapViewController?.labelCurrentRoad(at: location)
         }