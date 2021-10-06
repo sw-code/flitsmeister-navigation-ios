1f4468b3138b6a89e6227a7e195aa66b5a749a80
Dennis Wiosna <dennis.wiosna@gmail.com>
2021-10-06 14:23:42 +0200

    Forked all remaining mapbox libs and replaced mapbox with maplibre where possible

---
+++ MapboxSpeech/MapboxSpeech.h
@@ -0,0 +1,19 @@
+//
+//  MapboxSpeech.h
+//  MapboxSpeech
+//
+//  Created by Dennis Wiosna on 06.10.21.
+//  Copyright © 2021 Mapbox. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MapboxSpeech.
+FOUNDATION_EXPORT double MapboxSpeechVersionNumber;
+
+//! Project version string for MapboxSpeech.
+FOUNDATION_EXPORT const unsigned char MapboxSpeechVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MapboxSpeech/PublicHeader.h>
+
+
---
+++ Turf/Turf.h
@@ -0,0 +1,19 @@
+//
+//  Turf.h
+//  Turf
+//
+//  Created by Dennis Wiosna on 06.10.21.
+//  Copyright © 2021 Mapbox. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Turf.
+FOUNDATION_EXPORT double TurfVersionNumber;
+
+//! Project version string for Turf.
+FOUNDATION_EXPORT const unsigned char TurfVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Turf/PublicHeader.h>
+
+
