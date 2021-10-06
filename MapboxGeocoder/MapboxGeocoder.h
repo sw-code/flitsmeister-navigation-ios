b542e5bddd96c838ab9a1cd979fa60ee23fac9a2
Dennis Wiosna <dennis.wiosna@gmail.com>
2021-10-06 13:10:57 +0200

    Added maplibre as dependency and forked MapboxGeocoder

---
+++ MapboxGeocoder/MapboxGeocoder.h
@@ -0,0 +1,19 @@
+//
+//  MapboxGeocoder.h
+//  MapboxGeocoder
+//
+//  Created by Dennis Wiosna on 06.10.21.
+//  Copyright © 2021 Mapbox. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MapboxGeocoder.
+FOUNDATION_EXPORT double MapboxGeocoderVersionNumber;
+
+//! Project version string for MapboxGeocoder.
+FOUNDATION_EXPORT const unsigned char MapboxGeocoderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MapboxGeocoder/PublicHeader.h>
+
+
